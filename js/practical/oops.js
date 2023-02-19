9f7b4218b93baf9495860229ca68179c5e88d619
Saran Kumar R <93342856+sarankumar007@users.noreply.github.com>
2023-02-19 23:58:43 +0530

    Update oops.js

--- js/practical/oops.js
+++ js/practical/oops.js
@@ -1,1 +1,48 @@
 
+class Shape{
+    
+    constructor(colour) {
+        this.colour=colour;
+        
+    }
+    type="square";
+    describe(){
+        console.log(`A ${this.colour} ${this.type}`)
+    }
+}
+
+
+
+class Rectangle extends Shape{
+    type="rectangle"
+    constructor(colour,width,height)
+    {
+        super(colour);
+        this.width=width;
+        this.height=height;
+    }
+    area(){
+        return this.width*this.height;
+    }
+}
+class Square extends Shape{
+    constructor(colour,sidelength)
+    {
+        super(colour);
+        this.sidelength=sidelength;
+    }
+    area(){
+        return this.sidelength*this.sidelength;
+    }
+}
+
+const square = new Square("pink", 5);
+console.log( square.area() );
+
+const rectangle = new Rectangle("blue", 5,6);
+console.log( rectangle.area() );
+
+for(const shape of [square,rectangle])
+{
+    shape.describe();
+}
